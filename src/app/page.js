004f4cdb76884b450f691c46242eb5cf051e1cdb
EloM28 <mugisheloge@gmail.com>
2024-08-01 20:50:38 +0200

    delete env

--- src/app/page.js
+++ src/app/page.js
@@ -1,6 +1,8 @@
 'use client'
-
+import { SessionContext } from "@/components/context/Auth";
 export default function Home() {
+  const user = useContext(SessionContext);
+  const session = user.session;
   return (
     <>
       The Home Page
--- src/middleware.js
+++ src/middleware.js
@@ -1,15 +1,16 @@
 import { NextResponse } from "next/server"
+import { unstable_getServerSession } from 'next-auth/next';
 
-export function middleware(request) {
+export async function middleware(request) {
     const response = NextResponse.next()
     const url = request.nextUrl;
     const pathname = url.pathname;
     if (pathname === 'authentication/login' || pathname === 'authentication/register') {
         return response; // Allow access without authentication
     }
-    const user = ''
+    const session = await unstable_getServerSession(req, res, authOptions);
 
-    if (!user) {
+    if (!session) {
         return NextResponse.redirect(
             new URL('/authentication/login', request.url)
         )
